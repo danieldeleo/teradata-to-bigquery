8784da7bf930145b0240de2e536242e40f01b1fc
ddeleo <danieldeleo@google.com>
2024-05-19 17:59:05 -0400

    updates

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+# How to run TPT in Composer
+
+1. Create Composer Instance
+1. Get the Composer GKE Cluster credentials for executing kubectl
+   ```bash
+   gcloud container clusters get-credentials CLUSTER_NAME \
+     --region us-central1 \
+     --project YOUR_PROJECT_ID
+   ```
+1. [Create a Kubernetes secret](https://kubernetes.io/docs/tasks/configmap-secret/managing-secret-using-kubectl/#use-raw-data) in the Composer GKE Cluster using the following command:
+    ```bash
+    kubectl create secret generic tpt-secrets \
+      --namespace=composer-user-workloads \
+      --from-literal=TERADATA_PASSWORD=YOUR_TD_PASSWORD \
+      --from-literal=GCS_ACCESS_KEY_ID=srvcacct@YOUR_PROJECT_ID.iam.gserviceaccount.com \
+      --from-literal=GCS_SECRET_ACCESS_KEY='-----BEGIN PRIVATE KEY-----\n ... \n-----END PRIVATE KEY-----\n'
+    ```
+1. Copy the [tpt.py](composer/tpt.py) DAG into the Composer DAGs folder
 # How to run TPT in Cloud Build
 
 1. Create a private worker pool in Cloud Build named "tpt" in the us-central1 region which has network connectivity to your Teradata instance.
@@ -22,6 +40,9 @@ docker run \
   teradata/tpt:latest \
   -f /tmp/export.tpt \
   -u "jobvar_tdpid='10.128.0.26',jobvar_username='dbc',jobvar_password='pass'"
+
+
+  sudo docker run --entrypoint bash -v ./export.tpt:/tmp/export.tpt -v ./credentials.json:/root/.gcs/credentials -e "accept_license=Y,PASS=dbc"  teradata/tpt:latest -cx "tbuild -f /tmp/export.tpt -u \"jobvar_tdpid='10.128.0.26',jobvar_username='dbc',jobvar_password='${PASS}'\""
 ```
 
 # What's inside the TPT Docker Image?
---
+++ composer/tpt.py
@@ -0,0 +1,91 @@
+"""An example for running multi-tenant workloads"""
+import datetime
+
+import airflow
+from airflow import models
+from kubernetes.client import models as k8s
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+from airflow.providers.cncf.kubernetes.secret import Secret
+
+
+TERADATA_HOSTNAME = "10.128.0.26"
+TERADATA_USERNAME = "dbc"
+SELECT_STATEMENT = "SELECT * FROM tpch.orders;"
+GCS_BUCKET = "dannybq"
+GCS_PREFIX = "orders/"
+GCS_OBJECT_NAME = "data.csv"
+GCS_MAX_OBJECT_SIZE = "400M"
+GCS_CONNECTION_COUNT = "10"
+
+TERADATA_PASSWORD = Secret(
+    deploy_type="env",
+    deploy_target="TERADATA_PASSWORD",
+    secret="tpt-secrets",
+    key="TERADATA_PASSWORD",
+)
+
+GCS_ACCESS_KEY = Secret(
+    deploy_type="env",
+    deploy_target="GCS_ACCESS_KEY_ID",
+    secret="tpt-secrets",
+    key="GCS_ACCESS_KEY_ID",
+)
+
+GCS_SECRET_ACCESS_KEY = Secret(
+    deploy_type="env",
+    deploy_target="GCS_SECRET_ACCESS_KEY",
+    secret="tpt-secrets",
+    key="GCS_SECRET_ACCESS_KEY",
+)
+
+
+def read_export_tpt():
+    with open("/home/airflow/gcs/dags/export.tpt", "r") as f:
+        return f.read().replace("$", r"\$")
+
+
+with models.DAG(
+    dag_id="tpt",
+    default_args={"retries": 0},
+    schedule_interval=datetime.timedelta(days=1),
+    start_date=airflow.utils.dates.days_ago(1),
+    max_active_tasks=5,
+) as dag:
+    for x in range(1):
+        task_id = f"tpt{x}"
+        tpt = KubernetesPodOperator(
+            task_id=task_id,
+            name="tpt",
+            cmds=["bash"],
+            arguments=[
+                "-c",
+                rf"""
+                echo "{read_export_tpt()}" > export.tpt && \
+                more export.tpt && \
+                tbuild -f export.tpt -u "\
+                  jobvar_tdpid='{TERADATA_HOSTNAME}', \
+                  jobvar_username='{TERADATA_USERNAME}', \
+                  jobvar_password='${{TERADATA_PASSWORD}}', \
+                  jobvar_selectstmt='{SELECT_STATEMENT}', \
+                  jobvar_accessmoduleinitstr='\
+                  Bucket={GCS_BUCKET} \
+                  Prefix={GCS_PREFIX}{x}/ \
+                  Object={GCS_OBJECT_NAME} \
+                  MaxObjectSize={GCS_MAX_OBJECT_SIZE} \
+                  ConnectionCount={GCS_CONNECTION_COUNT}'"
+                """,
+            ],
+            container_resources=k8s.V1ResourceRequirements(
+                limits={
+                    "cpu": "100m",
+                    "memory": "512Mi",
+                }
+            ),
+            namespace="composer-user-workloads",
+            secrets=[TERADATA_PASSWORD, GCS_ACCESS_KEY, GCS_SECRET_ACCESS_KEY],
+            image="teradata/tpt:latest",
+            config_file="/home/airflow/composer_kube_config",
+            kubernetes_conn_id="kubernetes_default",
+        )
