77c4cbb7ba4a264a2a9e58f10a81242fc629b4e2
ddeleo <danieldeleo@google.com>
2025-02-06 17:21:23 -0500

    fix

--- composer/tests/test_dag_serialization.py
+++ composer/tests/test_dag_serialization.py
@@ -11,13 +11,13 @@ def dagbag():
 
 
 def test_dag_import_errors(dagbag):
-    assert dagbag.import_errors == {}, "No import errors should be found"
+    assert dagbag.import_errors == {}, "No import errors should be found."
 
 
 def test_dag_import_warnings(dagbag):
-    assert dagbag.captured_warnings == 0, "No warnings should be captured"
+    assert len(dagbag.captured_warnings) == 0, "No warnings should be found."
 
 def test_filename_matches_dag_id(dagbag):
     """Tests that filename matches dag_id"""
     for dag in dagbag.dags.values():
-        assert dag.dag_id == Path(dag.relative_fileloc).stem, "Filename does not match DAG ID"
+        assert dag.dag_id == Path(dag.relative_fileloc).stem, "Filename does not match DAG ID."
