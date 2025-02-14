6674b1b8f45ae6ec6b58db0f04b9c28b22074f87
ddeleo <danieldeleo@google.com>
2025-02-14 16:10:52 -0500

    test

--- composer/dags/custom_task_group_example.py
+++ composer/dags/custom_task_group_example.py
@@ -5,7 +5,7 @@ from custom_task_group_class import CustomTaskGroup
 @dag(schedule=None, catchup=False)
 def custom_task_group_example():
     ctg = CustomTaskGroup(
-        group_id="my_custom_task_group", files=["file1", "file2", "file3", "file4"]
+        group_id="my_custom_task_group", files=["file1", "file2", "file3", "file4", "file5"]
     )
 
 
