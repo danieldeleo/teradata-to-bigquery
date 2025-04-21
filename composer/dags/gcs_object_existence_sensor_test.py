6ed42b6b28edf0282ba32eb208834d076ba37190
ddeleo <danieldeleo@google.com>
2025-04-21 14:14:56 -0400

    update

--- composer/dags/gcs_object_existence_sensor_test.py
+++ composer/dags/gcs_object_existence_sensor_test.py
@@ -22,7 +22,7 @@ default_args = {
 # Define the DAG
 with DAG(
     dag_id="gcs_object_existence_sensor_test",
-    start_date=pendulum.datetime(2025, 4, 21, tz="America/New_York"), # Adjust start date as needed
+    start_date=pendulum.datetime(2025, 4, 21, tz="US/Eastern"), # Adjust start date as needed
     schedule="*/1 14 * * *", # Can be set to a schedule string like "@daily" or None for manual runs
     catchup=False,
     max_active_runs=1,
