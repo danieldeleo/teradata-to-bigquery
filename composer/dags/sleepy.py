9629817a6acda17285740584b614f57711184ef2
ddeleo <danieldeleo@google.com>
2025-07-01 13:16:17 -0400

    fix

--- composer/dags/sleepy.py
+++ composer/dags/sleepy.py
@@ -3,7 +3,6 @@
 import datetime
 
 import airflow
-from airflow import models
 from airflow.decorators import dag, task
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
