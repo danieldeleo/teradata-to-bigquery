d607b6b1af94ecf931fca7f73ef70f7914f9bb5d
ddeleo <danieldeleo@google.com>
2025-07-10 16:25:52 -0400

    fix

--- composer/dags/custom_task_group_example.py
+++ composer/dags/custom_task_group_example.py
@@ -3,7 +3,7 @@ from composer.dags.custom_sequential_task_group_class import CustomTaskGroup
 
 
 @dag(schedule=None, catchup=False)
-def custom_sequential_task_group_example():
+def custom_task_group_example():
     ctg = CustomTaskGroup(
         group_id="my_custom_task_group", files=["file1", "file2", "file3", "file4", "file5"]
     )
