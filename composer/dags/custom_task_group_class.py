5d02ce9bf74c5a8a801b6239c899a97a00d095a3
ddeleo <danieldeleo@google.com>
2025-07-10 16:23:14 -0400

    update

--- composer/dags/custom_task_group_class.py
+++ composer/dags/custom_task_group_class.py
@@ -28,42 +28,6 @@ class CustomTaskGroup(TaskGroup):
         def sequential_task_group(file):
             @task(max_active_tis_per_dag=1)
             def task_1(file, ti):
-                if ti.map_index > 0:
-
-                    def get_upstream_dependency(ti):
-                        return models.TaskInstance.get_task_instance(
-                            ti.dag_id,
-                            ti.run_id,
-                            f"my_custom_task_group.sequential_task_group.task_2",
-                            ti.map_index - 1,
-                        )
-
-                    dependency_ti = get_upstream_dependency(ti)
-                    while (
-                        dependency_ti.state is None
-                        or dependency_ti.state
-                        in [
-                            "scheduled",  # The scheduler has determined the Task’s dependencies are met and it should run
-                            "queued",  # The task has been assigned to an Executor and is awaiting a worker
-                            "running",  # The task is running on a worker (or on a local/synchronous executor)
-                            # "success", # The task finished running without errors
-                            "restarting",  # The task was externally requested to restart when it was running
-                            # "failed", # The task had an error during execution and failed to run
-                            # "skipped", # The task was skipped due to branching, LatestOnly, or similar.
-                            # "upstream_failed", # An upstream task failed and the Trigger Rule says we needed it
-                            "up_for_retry",  # The task failed, but has retry attempts left and will be rescheduled.
-                            "up_for_reschedule",  # The task is a Sensor that is in reschedule mode
-                            "deferred",  # The task has been deferred to a trigger
-                            # "removed", # The task has vanished from the DAG since the run started
-                        ]
-                    ):
-                        sleep(60)  # Poll every minute
-                        dependency_ti = get_upstream_dependency(ti)
-                    if dependency_ti in ["failed", "upstream_failed"]:
-                        raise AirflowFailException(
-                            "Upstream task failed in sequential dynamic task run."
-                        )
-
                 print(f"task_1: {file=}")
                 return file
 
--- composer/dags/custom_task_group_example.py
+++ composer/dags/custom_task_group_example.py
@@ -1,12 +1,12 @@
 from airflow.decorators import dag
-from custom_task_group_class import CustomTaskGroup
+from composer.dags.custom_sequential_task_group_class import CustomTaskGroup
 
 
 @dag(schedule=None, catchup=False)
-def custom_task_group_example():
+def custom_sequential_task_group_example():
     ctg = CustomTaskGroup(
         group_id="my_custom_task_group", files=["file1", "file2", "file3", "file4", "file5"]
     )
 
 
-custom_task_group_example()
+custom_sequential_task_group_example()
