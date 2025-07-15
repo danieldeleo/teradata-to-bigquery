9dc81e59c840ca8a05625d471f16129d5c770394
ddeleo <danieldeleo@google.com>
2025-07-15 17:57:00 -0400

    update

--- composer/dags/circular_conf_achilles_heel.py
+++ composer/dags/circular_conf_achilles_heel.py
@@ -1,44 +1,42 @@
-# Import necessary libraries
-from __future__ import annotations
-
 from airflow.models.dag import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python_operator import PythonOperator # type: ignore
-import json
-from copy import deepcopy
+from airflow.operators.python_operator import PythonOperator
 
 
-# Trigger this DAG with the following dynamic config:
-# {"steps":[{"name":"middle", "type":"middle", "params":{"steps":{}}}]}
 with DAG(
-        dag_id="circular_conf_achilles_heel",
-        start_date=days_ago(1),
-        schedule=None,
-        catchup=False,
-        max_active_runs=1,
-        default_args={
-            "retries": 0
-        },
-        description="Example of a DAG which can cause Airflow Scheduler to endlessly restart itself, rendering Composer inoperable.",
-    ) as dag:
+    dag_id="circular_conf_achilles_heel",
+    start_date=days_ago(1),
+    schedule=None,
+    catchup=False,
+    max_active_runs=1,
+    default_args={"retries": 0},
+    description="Example of a DAG which can cause Airflow Scheduler to endlessly restart itself, rendering Composer inoperable.",
+) as dag:
 
-    # Start task (optional, good practice)
-    start = EmptyOperator(task_id="start", dag=dag)    
+    start = EmptyOperator(task_id="start", dag=dag)
 
     def _create_circular_conf(**context):
-        context['dag_run'].conf['steps'] = context['dag_run'].conf
+        context["dag_run"].conf["steps"] = context["dag_run"].conf
 
     def _downstream_task(**context):
-        print('hello')
+        print("Help!")
 
-    create_circular_conf = PythonOperator(task_id="create_circular_conf", python_callable=_create_circular_conf)
+    create_circular_conf = PythonOperator(
+        task_id="create_circular_conf", python_callable=_create_circular_conf
+    )
 
-    downstream_task = PythonOperator(task_id="downstream_task", python_callable=_downstream_task)
+    downstream_task = PythonOperator(
+        task_id="downstream_task", python_callable=_downstream_task
+    )
 
     # End task (optional, good practice)
     end = EmptyOperator(task_id="end", dag=dag)
 
     # Define task dependencies
-    # start >> create_circular_conf >> downstream_task >> end
-    start >> create_circular_conf >> end
+    start >> create_circular_conf >> downstream_task >> end
+
+    # If you run the create_circular_conf task without a real downstream task
+    # (note that the "end" task is an EmptyOperator which doesn't actually get run)
+    # then the scheduler doesn't run into infinite sigterm loop.
+    # start >> create_circular_conf >> end
