a6b662ee804cb22b71fa7193e0c81c83b3bf9258
ddeleo <danieldeleo@google.com>
2025-07-22 12:03:09 -0400

    update

--- composer/dags/custom_parallel_task_group_example.py
+++ composer/dags/custom_parallel_task_group_example.py
@@ -1,12 +1,16 @@
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 from custom_parallel_task_group_class import CustomParallelTaskGroup
 
 
 @dag(schedule=None, catchup=False)
 def custom_parallel_task_group_example():
+    @task
+    def get_files():
+        return ["file1", "file2", "file3", "file4", "file5"]
     ctg = CustomParallelTaskGroup(
-        group_id="my_custom_task_group", files=["file1", "file2", "file3", "file4", "file5"]
+        group_id="my_custom_task_group"
     )
+    ctg.expand(files=get_files())
 
 
 custom_parallel_task_group_example()
