686a990c6525ced17d150ca68101765b6bb38433
ddeleo <danieldeleo@google.com>
2025-07-22 14:44:10 -0400

    update

---
+++ composer/dags/custom_sleepy_task_group_class.py
@@ -0,0 +1,30 @@
+from airflow import models
+from airflow.decorators import task, task_group
+from airflow.utils.task_group import TaskGroup
+
+
+class CustomSleepyTaskGroup(TaskGroup):
+    def __init__(self, group_id, seconds=0  **kwargs):
+        super().__init__(group_id=group_id, **kwargs)
+
+        @task(task_group=self)
+        def sleep_for(seconds):
+            from time import sleep
+            sleep(seconds)
+            return seconds
+        
+        @task(task_group=self)
+        def more_sleep_for(seconds):
+            from time import sleep
+            sleep(seconds)
+            return seconds
+        
+        @task(task_group=self)
+        def even_more_sleep_for(seconds):
+            from time import sleep
+            sleep(seconds)
+            return seconds
+        
+        even_more_sleep_for(more_sleep_for(sleep_for(seconds)))
+
+        
---
+++ composer/dags/custom_sleepy_task_group_example.py
@@ -0,0 +1,13 @@
+from airflow.decorators import dag, task
+from custom_sleepy_task_group_class import CustomSleepyTaskGroup
+
+files = ["file1", "file2", "file3", "file4", "file5"]
+
+@dag(schedule=None, catchup=False)
+def custom_sleepy_task_group_example():
+    @task
+    def get_sleepy_seconds():
+        return [5,5,5,5,5]
+    CustomSleepyTaskGroup(group_id="my_custom_sleepy_task_group").expand(seconds=get_sleepy_seconds())
+
+custom_sleepy_task_group_example()
