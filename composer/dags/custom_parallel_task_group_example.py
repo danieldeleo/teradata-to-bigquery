51974fbf0061b3a1c33aa563ce2548e879c8b9a2
ddeleo <danieldeleo@google.com>
2025-07-22 14:49:24 -0400

    update

--- composer/dags/custom_parallel_task_group_example.py
+++ composer/dags/custom_parallel_task_group_example.py
@@ -6,13 +6,13 @@ files = ["file1", "file2", "file3", "file4", "file5"]
 @dag(schedule=None, catchup=False)
 def custom_parallel_task_group_example():
     ctg = CustomParallelTaskGroup(
-        group_id="my_custom_task_group", files=files
+        group_id="my_custom_task_group", files=files, add_suffix_on_collision=True
     )
     ctg2 = CustomParallelTaskGroup(
-        group_id="my_custom_task_group", files=files
+        group_id="my_custom_task_group_2", files=files, add_suffix_on_collision=True
     )
     ctg3 = CustomParallelTaskGroup(
-        group_id="my_custom_task_group", files=files
+        group_id="my_custom_task_group_3", files=files, add_suffix_on_collision=True
     )
     ctg >> ctg2 >> ctg3
 
