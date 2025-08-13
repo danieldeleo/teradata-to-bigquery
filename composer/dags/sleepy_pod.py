2d307f0db317dcb43e6fb24b1e0a1e19ab4e858a
ddeleo <danieldeleo@google.com>
2025-08-13 10:51:08 -0400

    update

---
+++ composer/dags/sleepy_pod.py
@@ -0,0 +1,40 @@
+"""Example of a Composer DAG that runs a long-running (5min) KubernetesPodOperator with retries."""
+
+import datetime
+
+import airflow
+from airflow.decorators import dag
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+
+
+@dag(
+    schedule_interval=None,
+    start_date=airflow.utils.dates.days_ago(1),
+    default_args={
+        "retries": 10,
+        "retry_delay": datetime.timedelta(seconds=10),
+    },
+)
+def sleepy_pod():
+    KubernetesPodOperator(
+        task_id="sleep",
+        cmds=["bash"],
+        arguments=[
+            "-c",
+            r"""
+            set -e && \
+            echo "Try number: $AIRFLOW_RETRY_NUMBER" && \
+            echo "Sleeping for 5 minutes" && \
+            sleep 5m
+            """,
+        ],
+        env_vars={"AIRFLOW_RETRY_NUMBER": "{{ task_instance.try_number }}"},
+        image="gcr.io/google.com/cloudsdktool/cloud-sdk:latest",
+        container_resources=k8s.V1ResourceRequirements(
+            requests={
+                "cpu": "100m",
+                "memory": "64Mi",
+            }
+        ),
+    )
